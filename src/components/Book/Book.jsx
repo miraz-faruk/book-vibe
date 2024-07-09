9dcadb70a67b05a94d22cf18229b202f22eaec1a
mirazfaruk <mirazfaruk@gmail.com>
2024-07-10 03:24:30 +0600

    Book page layout

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,24 +1,30 @@
+import { FaRegStar } from "react-icons/fa";
+
+
 const Book = ({ book }) => {
 
-    const { image } = book;
+    const { image, bookName, author, tags, category, rating } = book;
     return (
-        <div className="grid grid-cols-3 gap-6">
-            <div className="card bg-base-100">
-                <figure>
-                    <img
-                        src={image}
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
+        <div className="card border p-6">
+            <figure className="rounded-2xl bg-[#F3F3F3] mb-6">
+                <img className="py-10 px-20 brightness-90"
+                    src={image}
+                    alt="books" />
+            </figure>
+            <div className="card-actions gap-3 mb-4">
+                <div className="bg-[#23BE0A0D] px-4 p-2 rounded-full text-[#23BE0A] text-base font-medium">{tags[0]}</div>
+                <div className="bg-[#23BE0A0D] px-4 p-2 rounded-full text-[#23BE0A] text-base font-medium">{tags[1]}</div>
+            </div>
+            <div className="mb-5">
+                <h2 className="card-title text-2xl font-bold heading-font mb-4">{bookName}</h2>
+                <p className="text-[#131313CC] font-medium">By : {author}</p>
+            </div>
+            <hr className="border border-dashed mb-5" />
+            <div className="flex justify-between text-[#131313CC] font-medium">
+                <p>{category}</p>
+                <div className="flex items-center gap-2">
+                    <p>{rating}</p>
+                    <FaRegStar></FaRegStar>
                 </div>
             </div>
         </div>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -13,7 +13,7 @@ const Books = () => {
     return (
         <div>
             <h2 className="text-xl md:text-[40px] font-bold text-center heading-font mb-9">Books</h2>
-            <div>
+            <div className="grid grid-cols-3 gap-6">
                 {
                     books.map(book => <Book key={book.bookId} book={book}></Book>)
                 }
