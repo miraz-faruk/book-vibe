ba47d51df34d983aa58070a6b16874d4dc9ca138
mirazfaruk <mirazfaruk@gmail.com>
2024-07-10 01:12:31 +0600

    Load Books data for the page

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,28 @@
+const Book = ({ book }) => {
+
+    const { image } = book;
+    return (
+        <div className="grid grid-cols-3 gap-6">
+            <div className="card bg-base-100">
+                <figure>
+                    <img
+                        src={image}
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Shoes!
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
+
 const Books = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('books.json')
+            .then(res => res.json())
+            .then(data => setBooks(data))
+    }, [])
+
     return (
         <div>
-            <h2>this is books section</h2>
+            <h2 className="text-xl md:text-[40px] font-bold text-center heading-font mb-9">Books</h2>
+            <div>
+                {
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
+                }
+            </div>
         </div>
     );
 };
